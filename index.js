f943d28216058d38829b59c4bc8057e01c05cfe9
Dave Beck <dbeck2@outlook.com>
2018-06-08 21:59:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function whisper(string) {
 function logShout(string) {
   return spyOn.toUpperCase
 }
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
