c3789297151853cfdb8ea841190578ba0b01181c
Dave Beck <dbeck2@outlook.com>
2018-06-08 22:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function logShout(string) {
 }
 function sayHiToGrandma(string) {
   return string.toLowerCase
-  if 'string' = lowerCase;
+  if 'string' = 'lowerCase';
 }
 
 /* describe('sayHiToGrandma(string)', function() {
@@ -26,15 +26,15 @@ function sayHiToGrandma(string) {
   })
 */
 var uppercase = "HELLO!"
- 
+
 uppercase.toUpperCase() === uppercase // true
- 
+
 var lowercase = 'hello!'
- 
+
 lowercase.toLowerCase() === lowercase // true
- 
+
 var mixedCase = 'Hi there!'
- 
+
 mixedCase.toLowerCase() === mixedCase // false
- 
+
 mixedCase.toUpperCase() === mixedCase // false
