10ac429ffb1ced62627daefe5b40589689612679
Dave Beck <dbeck2@outlook.com>
2018-06-08 22:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ function whisper(string) {
 function logShout(string) {
   return spyOn.toUpperCase
 }
+function sayHiToGrandma(string) {
+  return string === lowercase
+}
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
 
 var uppercase = "HELLO!"
 
